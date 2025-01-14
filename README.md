01a58de4ffd4c3ddc0e9463e8f663aa0ec21c53b
Dan Mann <dan.mann@followapp.care>
2025-01-14 23:21:25 +0000

    fix things up a bit

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# scribd-downloader
-Download your books from Scribd in PDF format for personal and offline use.
+# everand-downloader
+Download your books from Everand in PDF format for personal and offline use, and because fuck Everand.
 
 ### Please note:
-1) this is a very first version, so expect bugs to happen. Please report them [opening a new issue](https://github.com/evmer/scribd-downloader/issues).
-2) only Scribd **eBooks** are supported for now (no PDF Documents/Audiobooks etc.)
+1) this is a very first version, so expect bugs to happen. Please ~~report them opening a new issue~~ learn some Python and deal with it. :P
+2) only Everand **eBooks** are supported for now (no PDF Documents/Audiobooks etc.)
 
 ## Installation
 
@@ -19,24 +19,19 @@ Install Playwright for Python:
 
 ## Running
 
-Watch the [demonstration video](https://youtu.be/sbMb_EOFNTA).
-
 1) Run the script:
 
->$ python3 run.py [BOOK URL]
+>$ py fuck_everand.py
 
-2) A browser instance will open. Proceed with the login on Scribd and make sure to solve the captcha (if any). This step is required only for the first run. If you later want to login with another account, delete the session.json file and re-run the script.
+2) A browser instance will open. Proceed with the login on Everand and make sure to solve the captcha (if any). This step is required only for the first run. If you later want to login with another account, delete the session.json file and re-run the script. You may also have to do 2FA for each run.
 
-3) The script will start downloading the book:
+3) The script will scrape your saved books from Everand
 
-![image](https://user-images.githubusercontent.com/10036897/208117827-afc6087e-fc76-4bca-8138-be839113d666.png)
+3) The script will start downloading each Everand ebook type book - scribd stuff and PDFs are not supported, but all Everand ebooks including sheet music should work fine.
 
 ## TODO:
 - [X] Scale/reduce pdf page size
-- [ ] Render links in the PDFs
-- [ ] Add EPUB conversion feature
-- [ ] Add support for Documents
-- [ ] Add support for Audiobooks
+- [ ] Fuck Everand
 
 # DISCLAIMER:
-The code is not intended for piracy or unlawful re-sharing purposes. You can only download the books you have purchased for the sole purpose of personal use. I do not take responsibility for illegal use of the software.
+The code is not intended for piracy or unlawful re-sharing purposes. You can only download the books you have purchased for the sole purpose of personal use. I do not take responsibility for illegal use of the software. But fuck Everand.
---
+++ fuck_everand.py
@@ -0,0 +1,159 @@
+from playwright.sync_api import sync_playwright
+from PyPDF2 import PdfMerger
+
+import os
+import re
+import sys
+import time
+import shutil
+
+ZOOM = 0.625
+
+with sync_playwright() as playwright:
+	browser = playwright.chromium.launch(headless=False)
+	context = browser.new_context(storage_state="session.json" if 'session.json' in os.listdir('.') else None)
+
+	page = context.new_page()
+	page.goto('https://www.everand.com/login', wait_until='domcontentloaded')
+
+	page.locator("div.user_row").wait_for(state='attached', timeout=0)
+
+	print('Logged in successfully.')
+
+	storage = context.storage_state(path="session.json")
+	context.close()
+	browser.close()
+
+	books = []
+	browser = playwright.chromium.launch(headless=True)
+
+	context = browser.new_context(
+		storage_state=  "session.json",
+		viewport={'width': 1200, 'height': 1600},
+		ignore_https_errors = True,
+		user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
+	)
+	context.set_extra_http_headers({'Accept-Language': 'en-US,en;q=0.9'})
+
+	# get books list from saved - this only gets Everand ebooks, scribd pdfs aren't supported :(
+	page = context.new_page()
+	page.goto('https://www.everand.com/saved')
+	page.locator('li.auto__shared_react_cells_saved_book').nth(0).wait_for(state='attached')
+	for item in page.locator('li.auto__shared_react_cells_saved_book>div>a').all():
+		books.append(item.get_attribute("href"))
+
+	for book_url in books:
+		if (book_url.startswith("https://www.scribd.com")):
+			print(f"Scribd item {book_filename} not supported, skipping")
+			continue
+
+		# create cache dir
+		book_filename = book_url.split('/')[5]
+
+		if (os.path.isfile(f"{book_filename}.pdf")):
+			print(f"{book_filename} already downloaded, skipping")
+			continue
+
+		cache_dir = f'{os.getcwd()}/{book_filename}'
+		try:
+			os.mkdir(cache_dir)
+		except FileExistsError:
+			pass
+
+		print(f"Downloading {book_filename}")
+
+		try:
+			page = context.new_page()
+			page.goto(book_url + '?mode=standard')
+
+			if 'Browser limit exceeded' in page.content():
+				context.close()
+				browser.close()
+				sys.exit('You have tried to read this from too many computers or web browsers recently, and will need to wait up to 24 hours before returning to this book.')
+
+			font_style = ""
+
+			# open toc menu
+			page.locator('div.auto__books_menus_table_of_contents').wait_for(state='visible')
+			page.evaluate("() => document.querySelector('div.auto__books_menus_table_of_contents a').click()")
+			chapter_selector = page.locator('a.chapter_title')
+			chapter_selector.nth(0).wait_for(state='visible')
+
+			# retrieve the number of chapters
+			num_of_chapters = chapter_selector.count()
+
+			# load the first chapter
+			page.evaluate("() => document.querySelector('li.text_btn[data-idx=\"0\"]').click()")
+			chapter_no = 1
+
+			# to render the chapter pages and save them as pdf
+			render_page = context.new_page()
+			render_page.set_viewport_size({"width": 1200, "height": 1600})
+
+			while True:
+
+				page.locator('div.vertical_page[data-page="0"]').wait_for()
+
+				chapter_pages = page.locator('div.vertical_page')
+				number_of_chapter_pages = chapter_pages.count()
+
+				print(f'Downloading chapter {chapter_no}/{num_of_chapters} ({number_of_chapter_pages} pages)')
+
+				merger = PdfMerger()
+
+				page_no = 1
+
+				while True:
+
+					page_elem = chapter_pages.nth(page_no-1)
+					html = page_elem.inner_html()
+
+					# replace img urls
+					html = html.replace('src="/', 'src="https://www.everand.com/')
+
+					# set page size
+					match = re.findall('width: ([0-9.]+)px; height: ([0-9.]+)px;', html)[0]
+					width, height = float(match[0]), float(match[1])
+					style = f'@page {{ size: {width*ZOOM}px {height*ZOOM}px; margin: 0; }} @media print {{ html, body {{ height: {height*ZOOM}px; width: {width*ZOOM}px;}}}}'
+					html = re.sub('data-colindex="0" style="', 'data-colindex="0" x="', html)
+					html = re.sub('position: absolute.*?"', f'overflow: hidden; height: {height}px; width: {width}px; white-space: nowrap; zoom: {ZOOM};"', html)
+
+					# render page
+					content = f'<style>{style}{font_style}</style>{html}'
+					render_page.set_content(content)
+
+					# print pdf
+					pdf_file = f'{cache_dir}/{chapter_no}_{page_no}.pdf'
+					render_page.pdf(path=pdf_file, prefer_css_page_size = True)
+					merger.append(pdf_file)
+
+					if page_no == number_of_chapter_pages:
+						merger.write(f"{cache_dir}/{chapter_no}.pdf")
+						merger.close()
+						break
+
+					page_no += 1
+
+				if chapter_no == num_of_chapters:
+					break
+
+				page.evaluate("() => document.querySelectorAll('button.load_next_btn')[0].click()")
+
+				time.sleep(1)
+				chapter_no += 1
+
+			print('Merging PDF pages...')
+			merger = PdfMerger()
+
+			for chapter_no in range(1, num_of_chapters+1):
+				merger.append(f"{cache_dir}/{chapter_no}.pdf")
+
+			merger.write(f"{book_filename}.pdf")
+			merger.close()
+
+			# delete cache dir
+			shutil.rmtree(cache_dir)
+
+			print('Download completed, enjoy your book!')
+		except BaseException as be:
+			print(f"failed to download {book_filename}: {be}")
